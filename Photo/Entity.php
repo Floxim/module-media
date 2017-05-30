9d62ac3677f59cc335c4821ebf6bcda501197511
dubr <dubr.cola@gmail.com>
2017-05-30 20:20:04 +0300

    default box for photo

--- Photo/Entity.php
+++ Photo/Entity.php
@@ -4,5 +4,16 @@ namespace Floxim\Media\Photo;
 class Entity extends \Floxim\Main\Content\Entity
 {
 
-
+    public function getDefaultBoxFields() {
+        return array(
+            array(
+                'type' => 'image',
+                'keyword' => 'image',
+                ''
+            ),
+            array(
+                array('keyword' => 'description', 'template' => 'text_value')
+            )
+        );
+    }
 }
